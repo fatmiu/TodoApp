349c32782027c66b0db988fbeda5dcdcda77978a
Rocky Huang <rocky.huang@metropia.com>
2022-07-01 16:48:03 +0800

    initial commit

---
+++ app/src/main/java/com/miumiu/todo/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.miumiu.todo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.miumiu.todo.ui.add_edit_todo.AddEditTodoScreen
+import com.miumiu.todo.ui.theme.TODOTheme
+import com.miumiu.todo.ui.todo_list.TodoListScreen
+import com.miumiu.todo.util.Routes
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TODOTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.TODO_LIST,
+                ) {
+                    composable(Routes.TODO_LIST) {
+                        TodoListScreen(
+                            onNavigate = {
+                                navController.navigate(it.route)
+                            })
+                    }
+                    composable(
+                        route = Routes.ADD_EDIT_TODO + "?todoId={todoId}",
+                        arguments = listOf(
+                            navArgument(name = "todoId") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            }
+                        )
+                    ) {
+                        AddEditTodoScreen(onPopBackStack = {
+                            navController.popBackStack()
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/miumiu/todo/TodoApp.kt
@@ -0,0 +1,8 @@
+package com.miumiu.todo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TodoApp: Application() {
+}
